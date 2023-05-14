1f86363f247d4e69e19274bdc0d0931c3fff9d29
Aisha1495 <aishaalmamari6@gmail.com>
2023-05-14 01:39:08 -0700

    Account get/create

--- src/main/java/com/example/Salary/System/Controller/AccountController.java
+++ src/main/java/com/example/Salary/System/Controller/AccountController.java
@@ -1,11 +1,50 @@
 package com.example.Salary.System.Controller;
 
 
+import com.example.Salary.System.Models.Account;
+import com.example.Salary.System.Models.Employee;
+import com.example.Salary.System.Repository.AccountRepository;
+import com.example.Salary.System.Repository.EmployeeRepository;
+import com.example.Salary.System.Service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class AccountController {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+
+    @RequestMapping("account/create")
+    public void saveAccount () {
+        createAccount();
+    }
+
+    @RequestMapping ("account/get")
+    public List<Account> getAccount(){
+        return AccountService.getAccount();
+    }
+
+    private void createAccount() {
+ Account account=new Account();
+ account.setAccountType("CRIDIT");
+ account.setAccountNumber("2317866868865.09f");
+ account.setAccountHolderName("Malak");
+ account.setBankName("Muscat");
+ account.setBranchCode(255);
+ account.setSwiftCode("2554j");
+ account.setIsActive(true);
+ account.setId(144445L);
+ account.setCreatedDate(new Date());
+ AccountService.saveAccount(account);
+
+    }
 
 
 }
--- src/main/java/com/example/Salary/System/Service/AccountService.java
+++ src/main/java/com/example/Salary/System/Service/AccountService.java
@@ -1,7 +1,30 @@
 package com.example.Salary.System.Service;
 
+import com.example.Salary.System.Models.Account;
+import com.example.Salary.System.Models.Employee;
+import com.example.Salary.System.Repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+
+
 public class AccountService {
+    @Autowired
+    static
+    AccountRepository accountRepository;
+
+
+
+    public static void saveAccount(Account account) {
+        accountRepository.save(account);
+    }
+
+    public static List<Account> getAccount() {
+
+        return accountRepository.findAll();
+    }
+
 }

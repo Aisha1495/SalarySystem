453e12fc77f06e6bd228bac2caf372bb52af9eca
Aisha1495 <aishaalmamari6@gmail.com>
2023-05-14 01:56:01 -0700

    Salary get/create

--- src/main/java/com/example/Salary/System/Controller/SalaryController.java
+++ src/main/java/com/example/Salary/System/Controller/SalaryController.java
@@ -1,7 +1,38 @@
 package com.example.Salary.System.Controller;
 
+import com.example.Salary.System.Models.Account;
+import com.example.Salary.System.Models.Salary;
+import com.example.Salary.System.Repository.SalaryRepository;
+import com.example.Salary.System.Service.AccountService;
+import com.example.Salary.System.Service.SalaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Controller
+@RestController
 public class SalaryController {
+
+    @Autowired
+    SalaryService salaryService;
+
+
+    @RequestMapping("account/create")
+    public void saveSalary () {
+        createSalary();
+    }
+
+    @RequestMapping ("account/get")
+    public List<Salary> getSalary(){
+        return SalaryService.getSalary();
+    }
+
+    private void createSalary() {
+
+
+    }
+
 }
--- src/main/java/com/example/Salary/System/Service/SalaryService.java
+++ src/main/java/com/example/Salary/System/Service/SalaryService.java
@@ -1,8 +1,28 @@
 package com.example.Salary.System.Service;
 
 
+import com.example.Salary.System.Models.Employee;
+import com.example.Salary.System.Models.Salary;
+import com.example.Salary.System.Repository.SalaryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SalaryService {
+
+    @Autowired
+    static
+    SalaryRepository salaryRepository;
+
+
+    public void saveSalary(Salary salary) {
+        salaryRepository.save(salary);
+    }
+
+
+    public static List<Salary> getSalary() {
+        return salaryRepository.findAll();
+    }
 }
